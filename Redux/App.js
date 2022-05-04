e2771a9a1b63aff2fc15199c825ec24c0294e058
Gokula Kannan L <96760784+gokulakannan-tringapps@users.noreply.github.com>
2022-05-04 19:19:37 +0530

    Add files via upload

---
+++ Redux/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+
+function App() {
+  return (
+  	<div className="page">
+  		<div className="header_section">
+  			
+  			<header>
+  				<h1>Header</h1>
+  			</header>
+  		</div>
+  		<div className="Sidebar">
+  			<p>Menu 1</p>
+  			<br></br>
+  			<p>Menu 2</p>
+  			<br></br>
+  			<p>Menu 3</p>
+  		</div>
+  		<div className="Content">
+  			
+  			<p>Content Box</p>
+  		</div>
+  		<div className="footer_section">
+  			
+  			<footer>
+  				<h1>Footer</h1>
+  			</footer>
+  		</div>
+  	</div>
+  );
+}
+
+export default App;
