61c48391c4977ada025d2a9c910f06e5c66a1296
Gokula Kannan L <96760784+gokulakannan-tringapps@users.noreply.github.com>
2022-05-05 17:42:47 +0530

    Update App.js

--- Redux/App.js
+++ Redux/App.js
@@ -1,33 +1,13 @@
 
+import Layout from "./layout";
 import './App.css';
 
 function App() {
   return (
-  	<div className="page">
-  		<div className="header_section">
-  			
-  			<header>
-  				<h1>Header</h1>
-  			</header>
-  		</div>
-  		<div className="Sidebar">
-  			<p>Menu 1</p>
-  			<br></br>
-  			<p>Menu 2</p>
-  			<br></br>
-  			<p>Menu 3</p>
-  		</div>
-  		<div className="Content">
-  			
-  			<p>Content Box</p>
-  		</div>
-  		<div className="footer_section">
-  			
-  			<footer>
-  				<h1>Footer</h1>
-  			</footer>
-  		</div>
-  	</div>
+  	 <div className="page">
+  	 	 <Layout/>
+  	 	 
+  	 </div>
   );
 }
 
